7268570618093e557b46091aea281600cb15c6ce
yamad <yamad.linuxer@gmail.com>
2019-10-27 07:49:15 +0900

    雛形っぽいのを作った

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string>
 #include "dolphin.h"
 
 int main(int argc, char* argv[]) {
@@ -10,20 +9,21 @@ int main(int argc, char* argv[]) {
 	int _return = 0;		//戻り値を設定
 
 
-	if (_permit_root) {
-		printf("You need to run dolphin in ROOT (or use sudo).\n");
-		_return = 2;
-	} else if (!(argv[1])) {
+	install(argv[2]);
+
+
+	if (!(argv[1])) {
 		printf("COULDN'T FIND OPTION!!\n");
 		_return = 3;
+	} else if (_permit_root) {
+		printf("You need to run dolphin in ROOT (or use sudo).\n");
+		_return = 2;
 	}
 
 	if (_return) {
 		printf("\nERROR!!\n\n");
 		return _return;
 	}
-
-	install(argv[2]);
 
 	return 0;
 }
