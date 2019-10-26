4c24c02d8355636b518642923606277b56b0f7de
yamad <yamad.linuxer@gmail.com>
2019-10-27 05:58:00 +0900

    いろいろ改善

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dolphin
 The dolphin package manager on apt ;)
+
+## ERROR
+error 2 : no ROOT permission
+error 3 : miss command option
--- main.cpp
+++ main.cpp
@@ -5,23 +5,25 @@
 int main(int argc, char* argv[]) {
 
 	int _permit_root = geteuid();	//root権限あれば0になるやつ
-
-	int _return = 0;
-
+	int _return = 0;		//戻り値を設定
 
 
-	if (!(argv[1])) {
-		printf("COULDN'T FIND OPTION!!\n");
-		_return = 1;
-	} if (_permit_root) {
+
+	if (_permit_root) {
 		printf("You need to run dolphin in ROOT (or use sudo).\n");
-		_return = 1;
+		_return = 2;
+	} else if (!(argv[1])) {
+		printf("COULDN'T FIND OPTION!!\n");
+		_return = 3;
 	}
 
 	if (_return) {
 		printf("ERROR\n");
 		return _return;
 	}
+
+
+
 
 	printf("success dolphin !");
 	return 0;
