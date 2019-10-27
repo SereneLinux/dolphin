723d517ab10ec3134c4a79e644d653523fb87d7b
yamad <yamad.linuxer@gmail.com>
2019-10-27 11:37:00 +0900

    判定の精度上げた

--- dolphin.sh
+++ dolphin.sh
@@ -5,30 +5,44 @@ function help()
     echo "THIS IS HELP"
 }
 
+export S_FLAG=0
+export I_FLAG=0
+export R_FLAG=0
+export U_FLAG=0
+
 #OPT=`getopt "SIRU:h" $@` || usage 
 #set -- $OPT
 
-set -- `getopt "SIRUh" $@` || usage 
+set -- `getopt "SsIiRrUuHh" $@` || usage 
 
 while [ -- != "$1" ]; do
     case $1 in
 	-S)
 	    S_FLAG=1
 	    ;;
+        -s)
+            S_FLAG=1
+            ;;
 	-I)
 	    I_FLAG=1
 	    ;;
+        -i)
+            I_FLAG=1
+            ;;
 	-R)
 	    R_FLAG=1
 	    ;;
+        -r)
+            R_FLAG=1
+            ;;
 	-U)
 	    U_FLAG=1
 	    ;;
-	-h)
-	    help
-	    ;;
+        -u)
+            U_FLAG=1
+            ;;
     esac
     shift 
 done
 
-exec dolphin-bin $S_FLAG,$I_FLAG,$R_FLAG,$U_FLAG
+exec echo $S_FLAG,$I_FLAG,$R_FLAG,$U_FLAG
