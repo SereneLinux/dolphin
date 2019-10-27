b962ea6c4734ef4d7d865442fb8159cbbddbeb68
yamad <yamad.linuxer@gmail.com>
2019-10-27 10:53:28 +0900

    Frontend in CLI

---
+++ dolphin.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function help()
+{
+    echo "THIS IS HELP"
+}
+
+#OPT=`getopt "SIRU:h" $@` || usage 
+#set -- $OPT
+
+set -- `getopt "SIRUh" $@` || usage 
+
+while [ -- != "$1" ]; do
+    case $1 in
+	-S)
+	    S_FLAG=1
+	    ;;
+	-I)
+	    I_FLAG=1
+	    ;;
+	-R)
+	    R_FLAG=1
+	    ;;
+	-U)
+	    U_FLAG=1
+	    ;;
+	-h)
+	    help
+	    ;;
+    esac
+    shift 
+done
+
+exec dolphin-bin
