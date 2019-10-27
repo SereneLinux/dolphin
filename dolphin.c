c15f3e3e44f2470df48f1093e5ca3016d2d04d98
yamad <yamad.linuxer@gmail.com>
2019-10-27 10:40:20 +0900

    オプションとかはシェルで書く

--- dolphin.c
+++ dolphin.c
@@ -8,22 +8,15 @@ int main(int argc, char* argv[]) {
 
 	int _permit_root = geteuid();	//root権限あれば0が入るやつ
 	int _return = 0;		//エラーコード
-	char *hoge = 0;
-
+	int hoge = 0;
 
 	if (!(argv[1])) {
-		printf("(HELP HERE)\n");
-		_return = 3;
+		printf("(HELP WILL BE SHOWN HERE)\n");
+		return 3;
 	}
 
 
-
-	if (strchr(argv[1],(int)'-')) {	
-		if (!strstr(argv[1],"--")) {
-			printf("OPTION: %s\n",argv[1]);
-		}
-	}
-
+	
 
 	if (_permit_root) {
 		printf("You need to run dolphin in ROOT (or use sudo).\n");
